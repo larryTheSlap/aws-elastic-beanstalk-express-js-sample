4218db3463d8738e77b20a0a49cec3828f754c4c
aymane haddi <aymanox86@gmail.com>
2020-11-19 13:32:08 +0100

    Full Pipeline Test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('KHAWA mchi t7awa'));
+app.get('/', (req, res) => res.send('KHAWA mchi t7awa 23'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
